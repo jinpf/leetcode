7863f91dfaa10af1cd8f82e6921541b4439c6309
第肆人称 <jpflcj@sina.com>
2015-03-19 14:34:24 +0800

    a Brute force way to solve problem 5
    a Brute force way to solve problem 5 with exceed time limite

---
+++ c++/5/5.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+/**
+ * to see whether a string is Palindrome
+ */
+bool isPalindrome(string s) {
+  for (int i = 0; i <= s.size() / 2; ++i) {
+  	if (s[i] != s[s.size() - 1 - i])
+  		return false;
+  }
+  return true;
+}
+
+/**
+ * O(n^3) this answer would have: Time Limit Exceeded
+ */
+string longestPalindrome(string s) {
+  string longest = "";
+  int maxsize = 0;
+  for (int i = 0; i < s.size(); ++i) {
+  	for (int j = maxsize; j <= s.size() - i; ++j) {
+  		if (isPalindrome(s.substr(i, j))) {
+  			maxsize = j;
+  			longest = s.substr(i, j);
+  		}
+  	}
+  }
+  return longest;
+}
+
+int main () {
+  string s[3] = {"abcba", "abccba", "abcbd"};
+  for (int i = 0; i != 3; ++i) {
+  	cout << longestPalindrome(s[i]) << endl;
+  }
+}
